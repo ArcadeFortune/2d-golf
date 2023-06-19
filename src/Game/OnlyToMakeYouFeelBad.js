a797ee219477f14d8a5d35b912660737b50292ac
ArcadeFortune <alessionights@gmail.com>
2023-06-19 20:22:00 +0200

    feat: logs every mistake the player makes bc ofc.

---
+++ src/Game/OnlyToMakeYouFeelBad.js
@@ -0,0 +1,17 @@
+const log = () => { console.log(haha[Math.floor(Math.random() * haha.length)]) }
+const haha = [
+  "You missed a flagpole, c'mon do better.",
+  "How could you miss that?",
+  "You're not very good at this, are you?",
+  "Look at you, missing flagpoles like that, how many are you going to miss?",
+  "You should consider getting good.",
+  "Keep missing flagpoles like that.",
+  "And you want to beat the highscore?\nYou're going to have to do better than that.",
+  "Imagine missing the easiest flagpole there is.",
+  "Seeing you miss that flagpole, makes me wonder where your skill went.\nMaybe you never had any.",
+  "YOU MISSED ONE!",
+]
+
+export default function randomHaha() {
+  log()  
+}
